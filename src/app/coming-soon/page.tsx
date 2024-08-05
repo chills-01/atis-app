9466e86e5db3d8e22b0e280b0a9657df3370faa9
chills01 <cart.2801@gmail.com>
2024-08-05 22:09:35 +1000

    initial logo page

---
+++ src/app/coming-soon/page.tsx
@@ -0,0 +1,9 @@
+import { Box, Paper, Text } from "@mantine/core";
+
+export default function Page() {
+  return (
+    <Box bg="#b9dff9" h="100vh">
+      <Text c="white" fw={700} size="2rem" ta="center">SkySync</Text>
+    </Box>
+  );
+}
