e7987e65edac07494ab6419c29be09e9c5c68f9b
chills01 <cart.2801@gmail.com>
2024-08-10 22:09:10 +1000

    all text added

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -7,12 +7,29 @@ import {
 } from "@tabler/icons-react";
 
 const lexend = Lexend({ subsets: ["latin"] });
+const lexendSemiBold = Lexend({ weight: "600", subsets: ["latin"] });
 
 export default function Page() {
   return (
-    <Stack bg={"#4B33AD"} h={"100vh"} gap={0} px={"50px"}>
-      <Stack h={"80%"} bd={"1px solid white"}></Stack>
-      <Group h={"20%"} bd={"1px solid white"} justify="space-around">
+    <Stack bg={"#4B33AD"} h={"100vh"} gap={0} px={"40px"}>
+      <Stack
+        h={"80%"}
+        // bd={"1px solid white"}
+        align="center"
+        justify="space-around"
+        py={"80px"}
+      >
+        <Text className={lexendSemiBold.className} c={"white"} size="48px" ta="center">
+          SKYSYNC
+        </Text>
+        <Text className={lexend.className} c={"white"} size="20px" ta="center">
+          The weather sharing {<br />}app for pilots.
+        </Text>
+        <Text className={lexend.className} c={"white"} size="20px" ta="center">
+          COMING SOON
+        </Text>
+      </Stack>
+      <Group h={"20%"} justify="space-around">
         <a href="https://github.com/chills-01/atis-app" target="_blank">
           <IconBrandGithub size={44} color="white" stroke={1.25} />
         </a>
