07bdb3ebcda16d80de2f5b68b1b69fc25ef3aca5
chills01 <cart.2801@gmail.com>
2024-08-10 21:27:26 +1000

    footer links

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -1,9 +1,25 @@
-import { Box, Paper, Text } from "@mantine/core";
+import { Box, Group, Paper, Stack, Text } from "@mantine/core";
+import { Lexend } from "next/font/google";
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconPlaneDeparture,
+} from "@tabler/icons-react";
+
+const lexend = Lexend({ subsets: ["latin"] });
 
 export default function Page() {
   return (
-    <Box bg="#b9dff9" h="100vh">
-      <Text c="white" fw={700} size="2rem" ta="center">SkySync</Text>
-    </Box>
+    <Stack bg={"#4B33AD"} h={"100vh"} gap={0} px={"50px"}>
+      <Stack h={"80%"} bd={"1px solid white"}></Stack>
+      <Group h={"20%"} bd={"1px solid white"} justify="space-around">
+        <a href="https://github.com/chills-01/atis-app" target="_blank">
+          <IconBrandGithub size={44} color="white" stroke={1.25} />
+        </a>
+        <a href="" target="_blank">
+          <IconBrandLinkedin size={50} color="white" stroke={1.1} />
+        </a>
+      </Group>
+    </Stack>
   );
 }
